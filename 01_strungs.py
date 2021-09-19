471feaa15d77c671bdccdba669e80b19a71ddf8e
Kaushal Pareek <kaushal.pareek@navexglobal.com>
2021-09-19 15:46:43 +0530

    first commit

---
+++ 01_strungs.py
@@ -0,0 +1,9 @@
+name = 'kaushal is learning python '
+
+# print(name[1:5])  # string slicing
+
+# print(name.endswith(' '))
+# print(name.capitalize())
+# print(name)
+print(name.find('python'))
+# print(name.endswith(' '))
---
+++ practice.py
@@ -0,0 +1,4 @@
+a = int(input("Enter first number "))
+b = int(input("Enter second number 5"))
+
+print("Sum is ", a+b)
